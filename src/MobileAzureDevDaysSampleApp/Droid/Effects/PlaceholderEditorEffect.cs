52bc897db4e5f56219945b6202e452adcbd21ee2
June Cho <junecho.ucb@gmail.com>
2017-09-20 18:48:33 -0700

    PlaceholderEffect Android

---
+++ src/MobileAzureDevDaysSampleApp/Droid/Effects/PlaceholderEditorEffect.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MobileAzureDevDaysSampleApp.Droid.Effects;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(PlaceholderEditorEffect), nameof(PlaceholderEditorEffect))]
+
+namespace MobileAzureDevDaysSampleApp.Droid.Effects
+{
+    public class PlaceholderEditorEffect
+    {
+        public class PlacehoderEditorRenderer : EditorRenderer
+        {
+            protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+            {
+                base.OnElementChanged(e);
+
+                if (Element == null)
+                    return;
+
+                var element = (PlaceholderEditor)Element;
+
+                Control.Hint = element.Placeholder;
+                Control.SetHintTextColor(element.PlaceholderColor.ToAndroid());
+            }
+        }
+    }
+}
